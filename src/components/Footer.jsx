b5378313bee00c74ff9d4cb2d31c2e507399c2c5
dudafortuna <maria_e_fortuna@estudante.sesisenai.org.br>
2023-11-24 18:29:15 -0300

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,8 @@ function Footer() {
   return (
     <div className="bg-vermelho p-16 text-white flex" id='footer'>
       <p className="font-work-sans mt-2 mb-6 ml-20 absolute">Desenvolvido por Maria Eduarda Silva Fortuna</p>
-      <div className="flex place-content-between gap[1000px] ml-20 mt-32 mb-0">
+      <div className="ml-20 mt-32 mb-0">
       <p className="font-bold">ESCOLA SESI</p>
-      <p className="font-bold">SESI SENAI</p>
       </div>
       
     </div>
