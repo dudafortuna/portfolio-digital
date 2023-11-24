c5d5f6707c1a9360ef6e66a8f5de6ad1ccefa532
dudafortuna <maria_e_fortuna@estudante.sesisenai.org.br>
2023-11-24 18:40:52 -0300

    updating about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,11 @@ function About() {
       <div className="flex">
       <div className="flex flex-col lg:flex-row justify-center items-center w-[1200px] h-[520px] bg-gray-200 bg-opacity-60 rounded-lg mt-52">
         <p className="font-work-sans ml-10 mt-10 mr-10 text-vermelho font-inter font-normal text-base">
-          Eu nasci em Florianópolis, tenho 18 anos e sou estudante do Senai!
+          Oiê, meu nome é Maria Eduarda mas podem me chamar de Dudinha! 
           <br />
-          Atualmente estou no 3º ano do ensino médio, cursando também o curso{" "}
+          Nasci no Rio de janeiro, tenho 18 anos e sou estudante do Senai!
+          <br />
+          Atualmente estou no 3º ano do ensino médio, cursando também o curso
           <br />
           técnico integrado em análise e desenvolvimento de sistemas.
         </p>
